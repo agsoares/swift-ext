b6186e42e98ee0405562dab0f74d96c9f9521728
Adriano Soares <gsoares.adriano@gmail.com>
2019-02-03 14:43:26 -0200

    added <> operator

--- Operators.swift
+++ Operators.swift
@@ -19,3 +19,18 @@ public func <-<T> (lhs: inout T, rhs: Any?) -> T {
     lhs = _rhs
     return lhs
 }
+
+// MARK: Composition operator
+
+precedencegroup SingleTypeComposition {
+    associativity: left
+}
+
+infix operator <>: SingleTypeComposition
+
+public func <> <A: AnyObject>(f: @escaping (A) -> Void, g: @escaping (A) -> Void) -> (A) -> Void {
+    return { a in
+        f(a)
+        g(a)
+    }
+}
