6abee98e943ffa7bad42d76ae34e48af2094b44c
Adriano Soares <gsoares.adriano@gmail.com>
2019-04-15 01:45:21 -0300

    added keyboard helper

---
+++ KeyboardObserver.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+class KeyboardObserver {
+
+    typealias ConstraintObserver = (
+        constraint: NSLayoutConstraint,
+        noKeyboardConst: CGFloat,
+        keyboardConst: CGFloat
+    )
+
+    private static var instance = KeyboardObserver()
+    private var observers: [ConstraintObserver] = []
+
+    private init() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(KeyboardObserver.keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(KeyboardObserver.keyboardWillHide(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+
+    static func addConstraint(_ constraint: NSLayoutConstraint,
+                              noKeyboardConst: CGFloat,
+                              keyboardConst: CGFloat?) {
+
+        let constraint = ConstraintObserver(
+            constraint: constraint,
+            noKeyboardConst: noKeyboardConst,
+            keyboardConst: keyboardConst ?? noKeyboardConst
+        )
+
+        instance.observers.append(constraint)
+    }
+
+    static func removeConstraint(_ constraint: NSLayoutConstraint) {
+
+        instance.observers = instance.observers.filter({ $0.constraint != constraint })
+    }
+
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard
+            let keyboardHeight = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height,
+            let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
+        else {
+            return
+        }
+
+        observers.forEach { observer in
+            UIView.animate(withDuration: animationDuration) {
+                observer.constraint.constant = -(keyboardHeight + observer.keyboardConst)
+                (observer.constraint.firstItem as? UIView)?.superview?.layoutIfNeeded()
+            }
+        }
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        guard let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        observers.forEach { observer in
+            UIView.animate(withDuration: animationDuration) {
+                observer.constraint.constant = -(observer.noKeyboardConst)
+                (observer.constraint.firstItem as? UIView)?.superview?.layoutIfNeeded()
+            }
+        }
+    }
+}
